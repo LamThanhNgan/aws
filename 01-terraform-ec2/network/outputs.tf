38056d8146ffe52595d7cb8441f982b50763b90d
LamThanhNgan <lamthanhngan662607004@gmail.com>
2025-06-18 10:32:26 +0700

    feat(terraform): deploy nginx web on aws ec2

--- 01-terraform-ec2/network/outputs.tf
+++ 01-terraform-ec2/network/outputs.tf
@@ -2,5 +2,9 @@ output "vpc_ids" {
   value = aws_vpc.vpc.id
 }
 output "subnet_ids" {
-  value = aws_subnet.public[*].id
+  value = aws_subnet.public.id
+}
+
+output "ec2-sg-id" {
+  value = aws_security_group.ec2-sg.id
 }
--- 01-terraform-ec2/outputs.tf
+++ 01-terraform-ec2/outputs.tf
@@ -5,3 +5,7 @@ output "network_vpc_ids" {
 output "network_subnet_ids" {
   value = module.network.subnet_ids
 }
+
+output "ec2-public_ip" {
+  value = module.ec2.ec2-public-ip
+}
